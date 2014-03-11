52f08917c51a470dd5ca1e226d06ae34cf17048a
Roy Peled <royp@folloze.com>
2014-03-11 12:26:44 +0200

    first

---
+++ README.md
@@ -0,0 +1,118 @@
+angular-class-extender
+======================
+
+A lightweight angular module that helps working with RESTful models.
+I was researching restangular and didn't like the fact that it was dependant on lodash and the dact that the models weren't configurable.
+
+I liked ng-resources approach, but the implementation is horrible with nested objects.
+
+So here is my solution.
+
+
+## Usage
+To use this module you must add the JS file to your HTML and add `ngExtender` as a dependency to your app:
+```html
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.2/angular.min.js"></script>
+    <script src="angular-class-extender.js"></script>
+</head>
+<body ng-app="app">
+...
+
+```
+```js
+angular.module("app", ["ngExtender"]);
+```
+
+### Basic Inheritance
+```js
+function ParentClass($scope){
+  $scope.foo = function(){
+    // Do A
+  }
+}
+
+function HelperClass($scope){
+  $scope.help = function(){
+    // Get help!
+  }
+}
+
+function ChildClass($extend, $scope){
+  $extend($scope).with(ParentClass, HelperClass);
+}
+```
+Now calling `foo()` or `help()` from `ChildClass` would actually call `foo()` from `ParentClass` and do 'A' or `help()` from `HelperClass`. 
+
+### Overriding Methods and Calling Super Methods
+```js
+function ChildClass($extend, $scope){
+  var $super = $extend($scope).with(ParentClass);
+  
+  $scope.foo = function(){
+    
+    // Do B
+    
+    $super.foo();
+  }
+}
+```
+Calling `foo()` from `ChildClass` would perform the overidden method, and the `ParentClass` method by calling `$super`.
+
+### Implementing Abtract Methods
+```js
+function ParentClass($scope){
+  $scope.$abstract("bar");
+}
+
+function ChildClass($extend, $scope){
+  $extend($scope).with(ParentClass);
+  
+  $scope.bar = function(){
+    // Do C
+  }
+}
+```
+Using the `$abstract` syntax you are making sure that whichever class extended `ParentClass` it has to implement `bar()` method.
+Not doing so would result in an error:
+> Abstract method bar must be overriden in ChildClass!
+
+### Binded Event
+This handler is called after all the classes in your inheritance tree have been linked, so you can be sure you can access all of the inherited or implemented properies.
+```js
+function ParentClass($scope){
+  window.alert($scope.bar); // undefined
+
+  $scope.$binded(function(){
+    window.alert($scope.bar); // hello world!
+  });
+  
+  $scope.$abstract("bar");
+}
+
+function ChildClass($extend, $scope){
+  $extend($scope).with(ParentClass);
+  
+  $scope.bar = "hello world!"
+}
+```
+
+### Unit Testing
+Angular Class Extender now fully supports unit testing by using the injected class $extendedController instead of $controller
+
+```js
+it("should properly identify extended methods", function(){
+
+    var scope = $rootScope.$new();
+    scope.test = true;
+
+    $extendedController("ChildClass", {$scope: scope});
+    expect(scope.test).toEqual(true);
+    expect(scope.bar).toEqual("hello world!");
+    expect(scope.foo).toBeDefined();
+    
+});
+```
---
+++ src/angular-nested-resource.js
@@ -0,0 +1,81 @@
+/**!
+ * AngularJS Nested Resource
+ * @author  Roy  <peled.roy@gmail.com>
+ * @version 1.0.0
+ */
+(function() {
+    var angularNested = angular.module('ngNestedResource', []);
+
+    angularNested.factory('nestedResource', ['$resource', function($resource) {
+
+
+        function buildStep(currentRoute, actions){
+
+            var result = {};
+            var nested = {};
+
+            // Check if we have any nested objects in the resource
+            // If we found any, remove them and save in a different object
+            for(var key in actions){
+                if(actions[key].nested || actions[key].route){
+                    nested[key] = actions[key];
+                    delete actions[key];
+                }
+            }
+
+            // Create a new resource for the normal resource requests (resource API)
+            var resource = $resource(currentRoute, {}, actions);
+
+            // Add all methods from the resource to our result object
+            for(var key in actions){
+                result[key] = resource[key];
+            }
+
+            for(var key in nested){
+                // For each nested object, create a new route from the config
+                var futureRoute = currentRoute + (nested[key].route || "");
+                // Create a resource object for that route, replace the '@'
+                // symbol with a resource param placeholder, and pass the configurations
+                // to the resource
+                var nestedResource = $resource(futureRoute.replace("@", ":param"), {}, {
+                    get: {method: nested[key].method, isArray: nested[key].isArray}
+                });
+                // Loop through the nested objects inside this action
+                result[key] = fabricateStep(futureRoute, nested[key], nestedResource);
+            }
+
+            return result;
+        }
+
+        function fabricateStep(current, nest, nestedResource){
+            return function(mainParam, payloadObj){
+                // When this method is called with a param save a new route with the static param
+                // for a future nested object
+                if(typeof mainParam == "object"){
+                    payloadObj = mainParam;
+                    mainParam = "";
+                }
+                var resourceParams = {param: mainParam};
+                for(var key in payloadObj){
+                    resourceParams[key] = payloadObj[key];
+                }
+                var futureRoute = current.replace("@", mainParam);
+                var nestedResult = nestedResource.get(resourceParams);
+                nestedResult.$promise.then(function(resourceObject){
+                    for(var action in nestedSteps)
+                        resourceObject["$" + action] = nestedSteps[action];
+                })
+                var nestedSteps = buildStep(futureRoute, angular.copy(nest.nested));
+
+                for(var action in nestedSteps)
+                    nestedResult["$" + action] = nestedSteps[action];
+
+                return nestedResult;
+            }
+        }
+
+        return function(root, actions){
+            return buildStep(root, actions);
+        }
+    }]);
+})();
